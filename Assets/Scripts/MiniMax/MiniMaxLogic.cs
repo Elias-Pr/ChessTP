57ebc9a3349110b41d2b613bfcd50d9462e29692
Elias <eliaspr2002@gmail.com>
2024-04-02 11:17:46 +0200

    alphabeta2

--- Assets/Scripts/MiniMax/MiniMaxLogic.cs
+++ Assets/Scripts/MiniMax/MiniMaxLogic.cs
@@ -11,16 +11,17 @@ namespace MiniMax
     
         /*function minimax(node, depth, maximizingPlayer) is
         if depth = 0 or node is a terminal node then
-    return the heuristic value of node
-    if maximizingPlayer then
-    value := −∞
-    for each child of node do
-    value := max(value, minimax(child, depth − 1, FALSE))
-    else (* minimizing player *)
-    value := +∞
-    for each child of node do
-    value := min(value, minimax(child, depth − 1, TRUE))
-    return value*/
+        return the heuristic value of node
+        if maximizingPlayer then
+        value := −∞
+        for each child of node do
+        value := max(value, minimax(child, depth − 1, FALSE))
+        else (* minimizing player *)
+        value := +∞
+        for each child of node do
+        value := min(value, minimax(child, depth − 1, TRUE))
+        return value*/
+        
         public int MinimaxFunction(Node node, int depth, int alpha, int beta, bool maximizingPlayer)
             {
                 if (depth == 0 || node.IsTerminal)
@@ -38,7 +39,7 @@ namespace MiniMax
                         value = Math.Max(value, MinimaxFunction(childNode, depth - 1, alpha, beta, false));
                         alpha = Math.Max(alpha, value);
                         if (beta <= alpha)
-                            break; // Beta cutoff
+                            break; 
                     }
                     return value;
                 }
@@ -50,7 +51,7 @@ namespace MiniMax
                         value = Math.Min(value, MinimaxFunction(childNode, depth - 1, alpha, beta, true));
                         beta = Math.Min(beta, value);
                         if (beta <= alpha)
-                            break; // Alpha cutoff
+                            break;
                     }
                     return value;
                 }
